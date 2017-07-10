9d3ce499bcfef8a064c60cd1e5604b5bad1f2d00
balintvecsey <balint.vecsey@gmail.com>
2017-07-10 09:53:35 +0200

    add averageOfEvenNumbers() function

--- src/JumpInExercise.java
+++ src/JumpInExercise.java
@@ -2,5 +2,19 @@
  * Created by Bálint on 2017. 07. 10..
  */
 public class JumpInExercise {
-  
+
+  public static double averageOfEvenNumbers(int[] numbers) {
+    int count = 0;
+    int total = 0;
+
+    for (int i = 0; i < numbers.length; i++) {
+      if (numbers[i] % 2 != 0) {
+        count++;
+        total += numbers[i];
+      }
+    }
+
+    double avg = (double) total / (double) count;
+    return avg;
+  }
 }
